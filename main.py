254cb4dd3b0716c8652dc48d1e267020c91c22e5
super-onigiri-man <syunsuke030828@gmail.com>
2024-11-06 10:57:38 +0900

    サイトで公開できる最低限の形にした

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import requests
 import pandas as pd
 import datetime
 from datetime import timedelta, timezone
-from cmcrameri import cm
-import numpy as np
-import pydeck as pdk
+from cmcrameri import cm #色
+import numpy as np #アメダスデータの調整
+import pydeck as pdk #地図の描画
 import streamlit as st  # Streamlitをインポート
+import statistics #座標の中央値を求めるに使用
 
 # ストリームリット用ページ設定をスクリプトの最初に配置
 st.set_page_config(page_title="10分間雨量", layout="wide", initial_sidebar_state="collapsed")
@@ -39,6 +40,25 @@ def get_data():
     df2.drop(columns=['lat1', 'lat2', 'lon1', 'lon2'], inplace=True)
 
     result = pd.concat([df2, df], axis=1)
+
+    return result
+
+def temp_color(result):
+    result = result.dropna(subset=["気温"]) #欠損値があった場合はその観測点削除
+
+    colors = cm.hawaii_r(np.linspace(0, 1, 256))
+    rgb_colors = (colors[:, :3] * 255).astype(int).tolist()
+
+    # Now the min and max functions should work correctly
+    min_height = -5
+    max_height = 35
+    result["color"] = result["気温"].apply(
+        lambda h: rgb_colors[int(255 * ((h - min_height) / (max_height - min_height)))]
+    )
+
+    return result
+
+def pre24h_color(result):
     result = result.dropna(subset=["２４時間雨量"])
 
     colors = cm.hawaii_r(np.linspace(0, 1, 256))
@@ -53,45 +73,172 @@ def get_data():
     return result
 
 def main():
-    st.title("10分間雨量")
 
-    data = get_data()
-
-    layer = pdk.Layer(
-        "ColumnLayer",
-        data=data,
-        get_position=["lon", "lat"],
-        get_elevation='２４時間雨量',
-        elevation_scale=2500,
-        radius=2500,
-        elevation_range=[0, 500],
-        get_fill_color='color',
-        get_line_color=[0, 0, 0],
-        pickable=True,
-        auto_highlight=True,
-        extruded=True,
+    option = st.selectbox(
+    '表示させたい内容を選択してください', 
+    ['10分間降水量', '1時間降水量', '24時間降水量' , '気温']
     )
 
-    tooltip = {
-        "html": "地点名：<ruby>{kjName}<rt>{knName}</rt></ruby><br>気温：{気温}℃<br>10分間雨量：{１０分間雨量}mm<br>1時間雨量：{１時間雨量}mm<br>24時間雨量：{２４時間雨量}mm",
-        "style": {"background": "grey", "color": "white", "font-family": '"ヒラギノ角ゴ Pro W3", "Meiryo", sans-serif', "z-index": "5000"},
-    }
+    if st.button('実行'):
+        if option == '10分間降水量':
+            data = get_data()
+            data = pre24h_color(data)
+            layer = pdk.Layer(
+                "ColumnLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                get_elevation='１０分間雨量',
+                elevation_scale=2500,
+                radius=2500,
+                elevation_range=[0, 500],
+                get_fill_color='color',
+                get_line_color=[0, 0, 0],
+                pickable=True,
+                auto_highlight=True,
+                extruded=True,
+            )
 
-    view_state = pdk.ViewState(
-        longitude=137.5936,
-        latitude=36.047,
-        zoom=5,
-        min_zoom=1,
-        max_zoom=15,
-        pitch=50,
-        bearing=-0
-    )
+                
 
-    
+            tooltip = {
+                "html": "地点名：<ruby>{kjName}<rt>{knName}</rt></ruby><br>10分間雨量：{１０分間雨量}mm",
+                "style": {"background": "grey", "color": "white", "font-family": '"ヒラギノ角ゴ Pro W3", "Meiryo", sans-serif', "z-index": "5000"},
+            }
 
-    r = pdk.Deck(layer, tooltip=tooltip, initial_view_state=view_state)
+            view_state = pdk.ViewState(
+                longitude=statistics.median(data['lon']),
+                latitude=statistics.median(data['lat']),
+                zoom=5,
+                min_zoom=1,
+                max_zoom=15,
+                pitch=50,
+                bearing=-0
+            )
 
-    st.pydeck_chart(r)
+            r = pdk.Deck(layer, tooltip=tooltip, initial_view_state=view_state)
+
+            st.pydeck_chart(r)
+
+   
+
+    if option == '1時間降水量':
+            data = get_data()
+            data = pre24h_color(data)
+            layer = pdk.Layer(
+                "ColumnLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                get_elevation='１時間雨量',
+                elevation_scale=2500,
+                radius=2500,
+                elevation_range=[0, 500],
+                get_fill_color='color',
+                get_line_color=[0, 0, 0],
+                pickable=True,
+                auto_highlight=True,
+                extruded=True,
+            )
+
+                
+
+            tooltip = {
+                "html": "地点名：<ruby>{kjName}<rt>{knName}</rt></ruby><br>1時間雨量：{１時間雨量}mm",
+                "style": {"background": "grey", "color": "white", "font-family": '"ヒラギノ角ゴ Pro W3", "Meiryo", sans-serif', "z-index": "5000"},
+            }
+
+            view_state = pdk.ViewState(
+                longitude=statistics.median(data['lon']),
+                latitude=statistics.median(data['lat']),
+                zoom=5,
+                min_zoom=1,
+                max_zoom=15,
+                pitch=50,
+                bearing=-0
+            )
+
+            r = pdk.Deck(layer, tooltip=tooltip, initial_view_state=view_state)
+
+            st.pydeck_chart(r)
+
+
+    if option == '24時間降水量':
+            data = get_data()
+            data = pre24h_color(data)
+            layer = pdk.Layer(
+                "ColumnLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                get_elevation='２４時間雨量',
+                elevation_scale=2500,
+                radius=2500,
+                elevation_range=[0, 500],
+                get_fill_color='color',
+                get_line_color=[0, 0, 0],
+                pickable=True,
+                auto_highlight=True,
+                extruded=True,
+            )
+
+                
+
+            tooltip = {
+                "html": "地点名：<ruby>{kjName}<rt>{knName}</rt></ruby><br>24時間雨量：{２４時間雨量}mm",
+                "style": {"background": "grey", "color": "white", "font-family": '"ヒラギノ角ゴ Pro W3", "Meiryo", sans-serif', "z-index": "5000"},
+            }
+
+            view_state = pdk.ViewState(
+                longitude=statistics.median(data['lon']),
+                latitude=statistics.median(data['lat']),
+                zoom=5,
+                min_zoom=1,
+                max_zoom=15,
+                pitch=50,
+                bearing=-0
+            )
+
+            r = pdk.Deck(layer, tooltip=tooltip, initial_view_state=view_state)
+
+            st.pydeck_chart(r)
+
+    if option == '気温':
+            data = get_data()
+            data = temp_color(data)
+            layer = pdk.Layer(
+                "ColumnLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                get_elevation='気温',
+                elevation_scale=2500,
+                radius=2500,
+                elevation_range=[0, 500],
+                get_fill_color='color',
+                get_line_color=[0, 0, 0],
+                pickable=True,
+                auto_highlight=True,
+                extruded=True,
+            )
+
+                
+
+            tooltip = {
+                "html": "地点名：<ruby>{kjName}<rt>{knName}</rt></ruby><br>気温：{気温}℃",
+                "style": {"background": "grey", "color": "white", "font-family": '"ヒラギノ角ゴ Pro W3", "Meiryo", sans-serif', "z-index": "5000"},
+            }
+
+            view_state = pdk.ViewState(
+                longitude=statistics.median(data['lon']),
+                latitude=statistics.median(data['lat']),
+                zoom=5,
+                min_zoom=1,
+                max_zoom=15,
+                pitch=50,
+                bearing=-0
+            )
+
+            r = pdk.Deck(layer, tooltip=tooltip, initial_view_state=view_state)
+
+            st.pydeck_chart(r)
+
 
 if __name__ == "__main__":
     main()
